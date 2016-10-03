36ce9eea9bf4fe850749ddeb9210a9940623de52
Iam Programmer <iamprogrammerlk@gmail.com>
2016-10-03 07:21:29 +0530

    Initial commit

--- v16.10.02.22/IamJavascripts/IamGadgetLoader.js
+++ v16.10.02.22/IamJavascripts/IamGadgetLoader.js
@@ -2,26 +2,34 @@
 //IamGadgetLoader
 function IamGadgetLoader() {
 	var IamDocumentRoot = document.getElementsByTagName("*");
-	var IamNodeState, IamCloneNode, file;
+	var IamNodeState;
 	for (IamNodeState = 0; IamNodeState < IamDocumentRoot.length; IamNodeState++) {
 		if (IamDocumentRoot[IamNodeState].getAttribute("IamGadgetLoader")) {
-			IamCloneNode = IamDocumentRoot[IamNodeState].cloneNode(false);
-			file = IamDocumentRoot[IamNodeState].getAttribute("IamGadgetLoader");
-			var xhttp = new XMLHttpRequest();
-			xhttp.onreadystatechange = function() {
-				if (xhttp.readyState == 4 && xhttp.status == 200) {
+			var IamCloneNode = IamDocumentRoot[IamNodeState].cloneNode(false);
+			var IamXMLHttpRequestPath = "https://iamprogrammerlk.github.io";
+			switch (IamDocumentRoot[IamNodeState].getAttribute("IamGadgetLoader")) {
+					break;
+/* 				case "":
+					IamXMLHttpRequestPath = IamXMLHttpRequestPath + "";
+					break; */
+				default: 
+					IamXMLHttpRequestPath = IamXMLHttpRequestPath + "/Index.html";
+			}
+			var IamXMLHttpRequest = new XMLHttpRequest();
+			IamXMLHttpRequest.onreadystatechange = function() {
+				if (IamXMLHttpRequest.readyState == 4 && IamXMLHttpRequest.status == 200) {
 					IamCloneNode.removeAttribute("IamGadgetLoader");
-					IamCloneNode.innerHTML = xhttp.responseText;
+					IamCloneNode.innerHTML = IamXMLHttpRequest.responseText;
 					IamDocumentRoot[IamNodeState].parentNode.replaceChild(IamCloneNode, IamDocumentRoot[IamNodeState]);
 				}
 			}      
-			xhttp.open("GET", file, true);
-			xhttp.send();
+			IamXMLHttpRequest.open("GET", IamXMLHttpRequestPath, true);
+			IamXMLHttpRequest.send();
 			return;
 		}
 	}
 
-	console.log("IamGadgetLoader : Loaded");
+	console.log("IamGadgetLoader::Loaded");
 	
 	
 }
