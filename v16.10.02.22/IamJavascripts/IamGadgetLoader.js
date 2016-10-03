e1474de569e940b045d668ab9495d6ad8a396acb
Iam Programmer <iamprogrammerlk@gmail.com>
2016-10-03 06:46:55 +0530

    Initial commit

--- v16.10.02.22/IamJavascripts/IamGadgetLoader.js
+++ v16.10.02.22/IamJavascripts/IamGadgetLoader.js
@@ -1,7 +1,29 @@
 
 //IamGadgetLoader
 function IamGadgetLoader() {
+	var IamDocumentRoot = document.getElementsByTagName("*");
+	var IamNodeState, IamCloneNode, file;
+	for (IamNodeState = 0; IamNodeState < IamDocumentRoot.length; IamNodeState++) {
+		if (IamDocumentRoot[IamNodeState].getAttribute("IamGadgetLoader")) {
+			IamCloneNode = IamDocumentRoot[IamNodeState].cloneNode(false);
+			file = IamDocumentRoot[IamNodeState].getAttribute("IamGadgetLoader");
+			var xhttp = new XMLHttpRequest();
+			xhttp.onreadystatechange = function() {
+				if (xhttp.readyState == 4 && xhttp.status == 200) {
+					IamCloneNode.removeAttribute("IamGadgetLoader");
+					IamCloneNode.innerHTML = xhttp.responseText;
+					IamDocumentRoot[IamNodeState].parentNode.replaceChild(IamCloneNode, IamDocumentRoot[IamNodeState]);
+				}
+			}      
+			xhttp.open("GET", file, true);
+			xhttp.send();
+			return;
+		}
+	}
+
 	console.log("IamGadgetLoader : Loaded");
+	
+	
 }
 IamGadgetLoader();
 
