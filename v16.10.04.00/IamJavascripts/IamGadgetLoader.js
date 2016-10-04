3e658f2714c88da754077f8a28fc58e6a531b346
Iam Programmer <iamprogrammerlk@gmail.com>
2016-10-04 13:54:46 +0530

    Initial commit

--- v16.10.04.00/IamJavascripts/IamGadgetLoader.js
+++ v16.10.04.00/IamJavascripts/IamGadgetLoader.js
@@ -1,40 +1,11 @@
 //IamGadgetLoader
-function IamGadgetLoader() {
-	var IamDocumentRoot = document.getElementsByTagName("div");
-	var IamNodeState;
-	var IamXMLHttpRequestPath = "https://iamprogrammerlk.github.io";
+var IamGadgetLoader function () {
+	var IamGadgetLoader = document.getElementsByTagName("ins");
+	var IamGadgetLoaderCount;
+	for (IamGadgetLoaderCount = 0; IamGadgetLoaderCount < IamGadgetLoader.length; IamGadgetLoaderCount++) {
 	
-	for (IamNodeState = 0; IamNodeState < IamDocumentRoot.length; IamNodeState++) {
-		if (IamDocumentRoot[IamNodeState].getAttribute("IamGadgetLoader")) {
-			var IamParentNode = IamDocumentRoot[IamNodeState];
-			var IamCloneNode = IamDocumentRoot[IamNodeState].cloneNode(false);
-			
-			switch (IamParentNode.getAttribute("IamGadgetLoader")) {
-				case "IamAdcodeConverter":
-					IamXMLHttpRequestPath = IamXMLHttpRequestPath + "/IamAdcodeConverter/index.html";
-					break;
-/* 				case "":
-					IamXMLHttpRequestPath = IamXMLHttpRequestPath + "";
-					break; */
-				default: 
-					IamXMLHttpRequestPath = IamXMLHttpRequestPath + "/Index.html";
-			}
-			var IamXMLHttpRequest = new XMLHttpRequest();
-			IamXMLHttpRequest.onreadystatechange = function() {
-				if (IamXMLHttpRequest.readyState == 4 && IamXMLHttpRequest.status == 200) {
-						
-					IamCloneNode.removeAttribute("IamGadgetLoader");
-					IamCloneNode.innerHTML = IamXMLHttpRequest.responseText;
-					IamParentNode.parentNode.replaceChild(IamCloneNode, IamParentNode);
-				}
-			}      
-			IamXMLHttpRequest.open("GET", IamXMLHttpRequestPath, true);
-			IamXMLHttpRequest.send();
-		}
 	}
 
-	console.log("IamGadgetLoader::Loaded");
+	console.log("IamGadgetLoader::Loaded ", IamGadgetLoader.length);
 	
-	
-}
-IamGadgetLoader();
+};
