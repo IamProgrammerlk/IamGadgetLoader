21946cf4a373e312840ed6fbd3f7ffdfa0e1296b
Iam Programmer <iamprogrammerlk@gmail.com>
2016-10-04 14:40:44 +0530

    Initial commit

--- v16.10.04.00/IamJavascripts/IamGadgetLoader.js
+++ v16.10.04.00/IamJavascripts/IamGadgetLoader.js
@@ -3,9 +3,22 @@
 	var IamGadgetLoader = document.getElementsByTagName("ins");
 	var IamGadgetLoaderCount;
 	for (IamGadgetLoaderCount = 0; IamGadgetLoaderCount < IamGadgetLoader.length; IamGadgetLoaderCount++) {
-	
-	}
 
-	console.log("IamGadgetLoader::Loaded ", IamGadgetLoader.length);
+		switch (IamGadgetLoader[IamGadgetLoaderCount].getAttribute("IamGadgetLoader")) {
+			case "IamAdcodeConverter":
+				console.log("IamGadgetLoader::IamAdcodeConverter ", IamGadgetLoader.length);
+				break;
+ 			case "IamAdcodeConverter2":
+				console.log("IamGadgetLoader::IamAdcodeConverter2 ", IamGadgetLoader.length);
+				break;
+			case "IamAdcodeConverter3":
+				console.log("IamGadgetLoader::IamAdcodeConverter3 ", IamGadgetLoader.length);
+				break;
+			default: 
+				return;
+		}
+		console.log(IamGadgetLoader.length);
+		
+	}
 	
 })();
